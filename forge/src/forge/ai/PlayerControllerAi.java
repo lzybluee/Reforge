lzybluee/Reforge

commit bbec94bafb63a5bea0deacc5fb63e5dc2f283505

Show selected indicator when use Ugin's ultimate

forge/src/forge/ai/PlayerControllerAi.java
@@ -864,6 +864,34 @@ public class PlayerControllerAi extends PlayerController {
         }
         return ChangeZoneAi.chooseCardToHiddenOriginChangeZone(destination, origin, sa, fetchList, player, decider);
     }
+    
+    @Override
+    public CardCollection chooseCardsForZoneChange(ZoneType destination,
+            List<ZoneType> origin, SpellAbility sa, CardCollection fetchList, DelayedReveal delayedReveal,
+            String selectPrompt, boolean isOptional, Player decider, int changeNum) {
+
+        if (delayedReveal != null) {
+            reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
+        }
+        
+        Card card = null;
+        CardCollection collection = new CardCollection();
+        int i = 0;
+        do {
+        	card = ChangeZoneAi.chooseCardToHiddenOriginChangeZone(destination, origin, sa, fetchList, player, decider);
+        	if(card == null) {
+        		if (fetchList.isEmpty() || decider.getController().confirmAction(sa, PlayerActionConfirmMode.ChangeZoneGeneral, selectPrompt)) {
+        			break;
+        		}
+            } else {
+            	fetchList.remove(card);
+            	collection.add(card);
+            }
+        	i++;
+        } while (i < changeNum);
+        
+        return collection;
+    }
 
     @Override
     public void resetAtEndOfTurn() {

forge/src/forge/player/PlayerControllerHuman.java
@@ -1335,6 +1335,65 @@ public class PlayerControllerHuman
     public Card chooseSingleCardForZoneChange(final ZoneType destination, final List<ZoneType> origin, final SpellAbility sa, final CardCollection fetchList, final DelayedReveal delayedReveal, final String selectPrompt, final boolean isOptional, final Player decider) {
         return chooseSingleEntityForEffect(fetchList, delayedReveal, sa, selectPrompt, isOptional, decider);
     }
+    
+    @Override
+    public CardCollection chooseCardsForZoneChange(ZoneType destination,
+            List<ZoneType> origin, SpellAbility sa, CardCollection optionList, DelayedReveal delayedReveal,
+            String selectPrompt, boolean isOptional, Player targetedPlayer, int changeNum) {
+    	// Human is supposed to read the message and understand from it what to choose
+        if (optionList.isEmpty()) {
+            if (delayedReveal != null) {
+                reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
+            }
+            return null;
+        }
+        if (!isOptional && optionList.size() == 1) {
+            if (delayedReveal != null) {
+                reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
+            }
+            return optionList;
+        }
+
+        boolean canUseSelectCardsInput = true;
+        for (final GameEntity c : optionList) {
+            if (c instanceof Player) {
+                continue;
+            }
+            final Zone cz = ((Card)c).getZone();
+            // can point at cards in own hand and anyone's battlefield
+            final boolean canUiPointAtCards = cz != null && (cz.is(ZoneType.Hand) && cz.getPlayer() == player || cz.is(ZoneType.Battlefield));
+            if (!canUiPointAtCards) {
+                canUseSelectCardsInput = false;
+                break;
+            }
+        }
+
+        if (canUseSelectCardsInput) {
+            if (delayedReveal != null) {
+                reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
+            }
+            final InputSelectCardsFromList input = new InputSelectCardsFromList(this, isOptional ? 0 : 1, changeNum, optionList);
+            input.setCancelAllowed(isOptional);
+            input.setMessage(MessageUtil.formatMessage(selectPrompt, player, targetedPlayer));
+            input.showAndWait();
+            return new CardCollection(input.getSelected());
+        }
+
+        tempShow(optionList);
+        if (delayedReveal != null) {
+            tempShow(delayedReveal.getCards());
+        }
+        final GameEntityView result = getGui().chooseSingleEntityForEffect(selectPrompt, GameEntityView.getEntityCollection(optionList), delayedReveal, isOptional);
+        endTempShowCards();
+        
+        if (result instanceof CardView) {
+        	CardCollection collection = new CardCollection();
+        	collection.add(game.getCard((CardView)result));
+            return collection;
+        }
+
+        return null;
+    }
 
     @Override
     public boolean isGuiPlayer() {

forge/src/forge/game/ability/effects/ChangeZoneEffect.java
@@ -753,72 +753,78 @@ public class ChangeZoneEffect extends SpellAbilityEffect {
         final boolean champion = sa.hasParam("Champion");
         final boolean forget = sa.hasParam("ForgetChanged");
         final boolean imprint = sa.hasParam("Imprint");
-        final String selectPrompt = sa.hasParam("SelectPrompt") ? sa.getParam("SelectPrompt") : MessageUtil.formatMessage("Select a card from {player's} " + Lang.joinHomogenous(origin).toLowerCase(), decider, player);
+        String selectPrompt = sa.hasParam("SelectPrompt") ? sa.getParam("SelectPrompt") : MessageUtil.formatMessage("Select a card from {player's} " + Lang.joinHomogenous(origin).toLowerCase(), decider, player);
         final String totalcmc = sa.getParam("WithTotalCMC");
         int totcmc = AbilityUtils.calculateAmount(source, totalcmc, sa);
 
         CardCollection chosenCards = new CardCollection();
-        for (int i = 0; i < changeNum && destination != null; i++) {
-            if (sa.hasParam("DifferentNames")) {
-                for (Card c : chosenCards) {
-                    fetchList = CardLists.filter(fetchList, Predicates.not(CardPredicates.nameEquals(c.getName())));
+        
+        if(!sa.hasParam("DifferentNames") && !sa.hasParam("ShareLandType") && !sa.hasParam("AtRandom") && totalcmc == null && !defined) {
+        	selectPrompt = sa.hasParam("SelectPrompt") ? sa.getParam("SelectPrompt") : MessageUtil.formatMessage("Select " + changeNum + " card(s) from {player's} " + Lang.joinHomogenous(origin).toLowerCase(), decider, player);
+        	chosenCards = decider.getController().chooseCardsForZoneChange(destination, origin, sa, fetchList, delayedReveal, selectPrompt, !sa.hasParam("Mandatory"), decider, changeNum);
+        } else {
+            for (int i = 0; i < changeNum && destination != null; i++) {
+                if (sa.hasParam("DifferentNames")) {
+                    for (Card c : chosenCards) {
+                        fetchList = CardLists.filter(fetchList, Predicates.not(CardPredicates.nameEquals(c.getName())));
+                    }
                 }
-            }
-            if (sa.hasParam("ShareLandType")) {
-                for (final Card card : chosenCards) {
+                if (sa.hasParam("ShareLandType")) {
+                    for (final Card card : chosenCards) {
 
-                    fetchList = CardLists.filter(fetchList, new Predicate<Card>() {
-                        @Override
-                        public boolean apply(final Card c) {
-                            return  c.sharesLandTypeWith(card);
-                        }
+                        fetchList = CardLists.filter(fetchList, new Predicate<Card>() {
+                            @Override
+                            public boolean apply(final Card c) {
+                                return  c.sharesLandTypeWith(card);
+                            }
 
-                    });
+                        });
+                    }
                 }
-            }
-            if (totalcmc != null) {
-                if (totcmc >= 0) {
-                    fetchList = CardLists.getValidCards(fetchList, "Card.cmcLE" + Integer.toString(totcmc), source.getController(), source);
+                if (totalcmc != null) {
+                    if (totcmc >= 0) {
+                        fetchList = CardLists.getValidCards(fetchList, "Card.cmcLE" + Integer.toString(totcmc), source.getController(), source);
+                    }
                 }
-            }
 
-            Card c = null;
-            if (sa.hasParam("AtRandom")) {
+                Card c = null;
+                if (sa.hasParam("AtRandom")) {
+                    if (delayedReveal != null) {
+                        decider.getController().reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
+                    }
+                    c = Aggregates.random(fetchList);
+                }
+                else if (defined) {
+                    c = Iterables.getFirst(fetchList, null);
+                }
+                else {
+                    String title = selectPrompt;
+                    if (changeNum > 1) { //indicate progress if multiple cards being chosen
+                        title += " (" + (i + 1) + " / " + changeNum + ")";
+                    }
+                    c = decider.getController().chooseSingleCardForZoneChange(destination, origin, sa, fetchList, delayedReveal, title, !sa.hasParam("Mandatory"), decider);
+                }
+
+                if (c == null) {
+                    final int num = Math.min(fetchList.size(), changeNum - i);
+                    String message = "Cancel Search? Up to " + num + " more card" + (num != 1 ? "s" : "") + " can be selected.";
+
+                    if (fetchList.isEmpty() || decider.getController().confirmAction(sa, PlayerActionConfirmMode.ChangeZoneGeneral, message)) {
+                        break;
+                    }
+                    i--;
+                    continue;
+                }
+
+                fetchList.remove(c);
                 if (delayedReveal != null) {
-                    decider.getController().reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
+                    delayedReveal.remove(CardView.get(c));
                 }
-                c = Aggregates.random(fetchList);
-            }
-            else if (defined) {
-                c = Iterables.getFirst(fetchList, null);
-            }
-            else {
-                String title = selectPrompt;
-                if (changeNum > 1) { //indicate progress if multiple cards being chosen
-                    title += " (" + (i + 1) + " / " + changeNum + ")";
+                chosenCards.add(c);
+
+                if (totalcmc != null) {
+                    totcmc -= c.getCMC();
                 }
-                c = decider.getController().chooseSingleCardForZoneChange(destination, origin, sa, fetchList, delayedReveal, title, !sa.hasParam("Mandatory"), decider);
-            }
-
-            if (c == null) {
-                final int num = Math.min(fetchList.size(), changeNum - i);
-                String message = "Cancel Search? Up to " + num + " more card" + (num != 1 ? "s" : "") + " can be selected.";
-
-                if (fetchList.isEmpty() || decider.getController().confirmAction(sa, PlayerActionConfirmMode.ChangeZoneGeneral, message)) {
-                    break;
-                }
-                i--;
-                continue;
-            }
-
-            fetchList.remove(c);
-            if (delayedReveal != null) {
-                delayedReveal.remove(CardView.get(c));
-            }
-            chosenCards.add(c);
-
-            if (totalcmc != null) {
-                totcmc -= c.getCMC();
             }
         }
 

forge/src/forge/game/player/PlayerController.java
@@ -226,6 +226,7 @@ public abstract class PlayerController {
 
     // better to have this odd method than those if playerType comparison in ChangeZone  
     public abstract Card chooseSingleCardForZoneChange(ZoneType destination, List<ZoneType> origin, SpellAbility sa, CardCollection fetchList, DelayedReveal delayedReveal, String selectPrompt, boolean isOptional, Player decider);
+    public abstract CardCollection chooseCardsForZoneChange(ZoneType destination, List<ZoneType> origin, SpellAbility sa, CardCollection fetchList, DelayedReveal delayedReveal, String selectPrompt, boolean isOptional, Player decider, int changeNum);
 
     public abstract void autoPassCancel();
 

commit d8c4db617a3ae8948fa0668051463fd9d0b9769b

Fix bug introduced in bbec94bafb63a5bea0deacc5fb63e5dc2f283505

forge/src/forge/ai/PlayerControllerAi.java
@@ -874,11 +874,10 @@ public class PlayerControllerAi extends PlayerController {
             reveal(delayedReveal.getCards(), delayedReveal.getZone(), delayedReveal.getOwner(), delayedReveal.getMessagePrefix());
         }
         
-        Card card = null;
         CardCollection collection = new CardCollection();
         int i = 0;
         do {
-        	card = ChangeZoneAi.chooseCardToHiddenOriginChangeZone(destination, origin, sa, fetchList, player, decider);
+        	Card card = ChangeZoneAi.chooseCardToHiddenOriginChangeZone(destination, origin, sa, fetchList, player, decider);
         	if(card == null) {
         		if (fetchList.isEmpty() || decider.getController().confirmAction(sa, PlayerActionConfirmMode.ChangeZoneGeneral, selectPrompt)) {
         			break;

forge/src/forge/player/PlayerControllerHuman.java
@@ -1379,20 +1379,30 @@ public class PlayerControllerHuman
             return new CardCollection(input.getSelected());
         }
 
-        tempShow(optionList);
-        if (delayedReveal != null) {
-            tempShow(delayedReveal.getCards());
-        }
-        final GameEntityView result = getGui().chooseSingleEntityForEffect(selectPrompt, GameEntityView.getEntityCollection(optionList), delayedReveal, isOptional);
-        endTempShowCards();
-        
-        if (result instanceof CardView) {
-        	CardCollection collection = new CardCollection();
-        	collection.add(game.getCard((CardView)result));
-            return collection;
-        }
+        CardCollection collection = new CardCollection();
+        int i = 0;
+        do {
+        	tempShow(optionList);
+            if (delayedReveal != null) {
+                tempShow(delayedReveal.getCards());
+            }
+            final GameEntityView result = getGui().chooseSingleEntityForEffect(selectPrompt, GameEntityView.getEntityCollection(optionList), delayedReveal, isOptional);
+            endTempShowCards();
 
-        return null;
+        	if(result == null) {
+        		if (optionList.isEmpty() || confirmAction(sa, PlayerActionConfirmMode.ChangeZoneGeneral, selectPrompt)) {
+        			break;
+        		}
+            } else {
+				if (result instanceof CardView) {
+					collection.add(game.getCard((CardView)result));
+					optionList.remove(game.getCard((CardView)result));
+				}
+            }
+        	i++;
+        } while (i < changeNum);
+
+        return collection;
     }
 
     @Override