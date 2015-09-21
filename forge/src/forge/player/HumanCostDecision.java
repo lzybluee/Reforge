af3b3984173fed56b2d571311e2c0eba19215eb9
lzy <lzybluee@gmail.com>
2015-09-21 23:43:00 +0800

    Skip select zero target choice

--- forge/src/forge/player/HumanCostDecision.java
+++ forge/src/forge/player/HumanCostDecision.java
@@ -1047,7 +1047,9 @@ public class HumanCostDecision extends CostDecisionMakerBase {
         final InputSelectCardsFromList inp = new InputSelectCardsFromList(controller, c, c, list);
         inp.setMessage("Select a " + cost.getDescriptiveType() + " to sacrifice (%d left)");
         inp.setCancelAllowed(true);
-        inp.showAndWait();
+        if(c > 0) {
+        	inp.showAndWait();
+        }
         if (inp.hasCancelled()) {
             return null;
         }
@@ -1151,7 +1153,9 @@ public class HumanCostDecision extends CostDecisionMakerBase {
         final InputSelectCardsFromList inp = new InputSelectCardsFromList(controller, c, c, typeList);
         inp.setCancelAllowed(true);
         inp.setMessage("Select a " + cost.getDescriptiveType() + " to tap (%d left)");
-        inp.showAndWait();
+        if(c > 0) {
+        	inp.showAndWait();
+        }
         if (inp.hasCancelled()) {
             return null;
         }
@@ -1180,7 +1184,9 @@ public class HumanCostDecision extends CostDecisionMakerBase {
         final InputSelectCardsFromList inp = new InputSelectCardsFromList(controller, c, c, typeList);
         inp.setCancelAllowed(true);
         inp.setMessage("Select a " + cost.getDescriptiveType() + " to untap (%d left)");
-        inp.showAndWait();
+        if(c > 0) {
+        	inp.showAndWait();
+        }
         if (inp.hasCancelled() || inp.getSelected().size() != c) {
             return null;
         }
